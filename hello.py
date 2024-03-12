e95c71986490a69ee4f186e9c8136ab6c90ceae7
Kyle Matuszczak <kyle.l.matuszczak@gmail.com>
2024-03-11 21:33:01 -0400

    Added command line parsing

--- hello.py
+++ hello.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python3
 
+from sys import argv
 import pytesseract
 
+
 def main():
-    print(pytesseract.image_to_string('hello.png'))
+    if 1 == len(argv):
+        print("Requires .png file")
+        return 1
+
+    if not argv[1].endswith(".png"):
+        print("Must be .png file")
+        return 2
+
+    print(pytesseract.image_to_string(argv[1]))
 
 if __name__ == '__main__':
     try:
